f2faf34ac4f4cf0ef4874193ca331ecaaf3e8d81
Just A Guy <eugenerobert76@gmail.com>
2024-09-05 16:49:20 +0300

    caca

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
